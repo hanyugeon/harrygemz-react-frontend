95faf2383934cf0bb66ac2ea0ffdd1b769c97ef6
hanyugeon <hanyugeon@naver.com>
2022-06-21 23:40:51 +0900

    Feat: NFT 메타데이터 가져오기

--- components/MintingModal.tsx
+++ components/MintingModal.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
-import { useAccount, useCaver } from '../hooks';
+import { FC, useEffect } from 'react';
+import { useAccount, useCaver, useMetadata } from '../hooks';
 import { 
   Button,
   Modal,
@@ -12,6 +12,7 @@ import {
   Text
 } from '@chakra-ui/react';
 import { MINT_GEM_TOKEN_ADDRESS } from '../caverConfig';
+import { GemTokenData } from '../interfaces';
 
 
 interface MintingModalProps {
@@ -20,13 +21,16 @@ interface MintingModalProps {
 }
 
 const MintingModal: FC<MintingModalProps> = ({ isOpen, onClose }) => {
+  // hooks
   const { account } = useAccount();
-  const { caver, mintGemTokenContract } = useCaver();
+  const { caver, mintGemTokenContract, saleGemTokenContract } = useCaver();
+  const { metadataURI, getMetadata } = useMetadata();
 
   const onClickMint = async () => {
     try {
       if(!account || !caver || !mintGemTokenContract)  return;
 
+      // caver-js의 버그, send함수가 kaikas지갑이랑 같이 사용이 안됨
       // const response = await mintGemTokenContract.methods.mintGemToken().send({
       //   from: account,
       //   value: caver.utils.convertToPeb(1, "KLAY"),
@@ -42,11 +46,23 @@ const MintingModal: FC<MintingModalProps> = ({ isOpen, onClose }) => {
         data: mintGemTokenContract.methods.mintGemToken().encodeABI(),
       });
 
+      if (response) {
+        const latestMintedGemToken: GemTokenData = await saleGemTokenContract.methods
+        .getLatestMintedGemToken(account)
+        .call()
+
+        getMetadata(latestMintedGemToken.gemTokenRank, latestMintedGemToken.gemTokenType);
+
+        console.log(latestMintedGemToken);
+      }
+
       console.log(response)
     } catch(error) {
       console.log(error);
     }
   };
+
+  useEffect(() => console.log(metadataURI), [metadataURI])
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>

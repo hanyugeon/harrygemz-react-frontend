7a3a22e50a15959a09634fba4031dff4b0d2e0b4
hanyugeon <hanyugeon@naver.com>
2022-06-26 02:19:12 +0900

    Feat: Layout 코드 작성

---
+++ components/Layout.tsx
@@ -0,0 +1,19 @@
+import { FC, ReactNode } from 'react';
+import { Box } from '@chakra-ui/react';
+
+import Header from './Header'
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: FC<LayoutProps> = ({ children }) => {
+  return (
+  <Box>
+    <Header />
+    {children}
+  </Box>
+  );
+};
+
+export default Layout;
---
+++ pages/my-gemz.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Box } from "@chakra-ui/react";
+
+const MyGemz: FC = () => {
+  return (
+  <Box>my-gemz</Box>
+  );
+}
+
+export default MyGemz;

61349aa741ec582a0ac3cdfba67ae59e745f7645
hanyugeon <hanyugeon@naver.com>
2022-06-29 22:56:20 +0900

    Feat: NFT 판매 기능 구현

--- components/MyGemCard.tsx
+++ components/MyGemCard.tsx
@@ -1,21 +1,96 @@
-import { FC, useEffect } from 'react';
+import { FC, useState, useEffect } from 'react';
+import { 
+  Box, 
+  Button, 
+  Flex, 
+  Input, 
+  InputGroup, 
+  InputRightAddon, 
+  Text 
+} from '@chakra-ui/react';
 import { GemTokenData } from '../interfaces'
-import { useMetadata } from '../hooks';
+import { useAccount, useCaver, useMetadata } from '../hooks';
 import GemCard from "./GemCard";
+import { SALE_GEM_TOKEN_ADDRESS } from '../caverConfig';
+
 
 interface MyGemCardProps {
   gemTokenData: GemTokenData;
 }
 
 const MyGemCard: FC<MyGemCardProps> = ({ gemTokenData }) => {
+  const [sellPrice, setSellPrice] = useState<string>("");
+  const [myGemPrice, setMyGemPrice] = useState<string>(gemTokenData.tokenPrice);
+
+  const { account } = useAccount();
+  const { caver, saleGemTokenContract } = useCaver();
   const { metadataURI, getMetadata } = useMetadata();
+
+  const onClickSell = async () => {
+    try {
+      if(!account || !caver || !saleGemTokenContract) return ;
+
+      const response = await caver.klay.sendTransaction({
+        type: "SMART_CONTRACT_EXECUTION", 
+        from: account, 
+        to: SALE_GEM_TOKEN_ADDRESS, 
+        gas: "3000000", 
+        data: saleGemTokenContract.methods
+          .setForSaleGemToken(
+            gemTokenData.tokenId, 
+            caver.utils.convertToPeb(sellPrice, "KLAY")
+          )
+          .encodeABI(), 
+      });
+
+      if (response.status) {
+        setMyGemPrice(caver.utils.convertToPeb(sellPrice, "KLAY"));
+      }
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   useEffect(() => {
     getMetadata(gemTokenData.gemTokenRank, gemTokenData.gemTokenType);
   }, []);
   
   return (
-    <GemCard metadataURI={metadataURI} />
+    <Box w={200} m={2}>
+      <GemCard metadataURI={metadataURI}/>
+        {myGemPrice === "0" ? (
+          <Flex>
+            <InputGroup size="sm" borderColor="#E88CBD">
+              <Input 
+                type="number"
+                borderWidth={2}
+                borderColor="#E88CBD"
+                focusBorderColor="#E88CBD"
+                value={sellPrice}
+                onChange={e => setSellPrice(e.target.value)}
+              />
+              <InputRightAddon 
+                bg="#E88CBD" 
+                color="#FFFFFF" 
+                fontSize={16} 
+                children="Klay"
+              />
+            </InputGroup>
+            <Button 
+              size="sm"
+              ml={2}
+              bg="#E88CBD"
+              color="#FFFFFF"
+              onClick={onClickSell}
+            >
+              Sell
+            </Button>
+          </Flex>
+        ) : (
+          <Text>{caver?.utils.convertFromPeb(myGemPrice, "KLAY")} KLAY</Text>
+        )}
+    </Box>
   );
 }
 
